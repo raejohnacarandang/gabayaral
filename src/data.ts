607c6c475df0872b137eff2f902895664b320d93
raejohnacarandang <raejohncarandang121303@gmail.com>
2026-05-05 23:17:27 +0800

    Initial commit

---
+++ src/data.ts
@@ -0,0 +1,17 @@
+import { Student, Subject, GradeEntry, TeacherFeedback, Alert } from './types';
+
+export const MOCK_STUDENTS: Student[] = [];
+
+export const MOCK_SUBJECTS: Subject[] = [
+  { id: 'sub1', name: 'Mathematics', teacherId: 't1' },
+  { id: 'sub2', name: 'Science', teacherId: 't1' },
+  { id: 'sub3', name: 'English', teacherId: 't1' },
+  { id: 'sub4', name: 'Filipino', teacherId: 't1' },
+];
+
+export const MOCK_GRADES: GradeEntry[] = [];
+
+export const MOCK_FEEDBACK: TeacherFeedback[] = [];
+
+export const MOCK_ALERTS: Alert[] = [];
+
